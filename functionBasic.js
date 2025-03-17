0002a18adcc739d1a5c84b2e924b585d896c5b8d
Nedrey Jon M. Golosino <ngolosino15@gmail.com>
2025-03-17 20:58:41 +0800

    Added More Functions

--- functionBasic.js
+++ functionBasic.js
@@ -9,3 +9,28 @@ function addNumbers(a, b){
 }
 
 console.log(addNumbers(3,4));
+
+function add7(a){
+    return a + 7;
+}
+
+console.log(add7(1));
+
+function multiplyNumbers(a,b){
+    return a * b;
+}
+
+console.log(multiplyNumbers(4, 5));
+
+function capitalize(val){
+    return String(val[0]).toUpperCase() + String(val).slice(1);
+}
+
+console.log(capitalize("hello world"));
+
+function lastLetter(val){
+    return String(val.slice(0,-1) + "d");
+}
+
+
+console.log(lastLetter("hello yawars"))
