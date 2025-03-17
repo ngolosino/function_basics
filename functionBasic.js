be80f0c6fc589297fb42694482e7edc738c66b56
Nedrey Jon M. Golosino <ngolosino15@gmail.com>
2025-03-17 20:39:03 +0800

    Added Number Changes

--- functionBasic.js
+++ functionBasic.js
@@ -2,4 +2,10 @@ function favoriteAnimal(animal){
     return animal + " is my favorite animal";
 }
 
-    console.log(favoriteAnimal("Cats"));
+console.log(favoriteAnimal("Cats"));
+
+function addNumbers(a, b){
+    return a + b;
+}
+
+console.log(addNumbers(3,4));
